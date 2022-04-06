c98bc80e2fd7a3a2d035e8afc68aebe4d42b96ca
Gus <gusmcg1856@gmail.com>
2022-04-06 11:09:30 -0600

    Gravitational Force

---
+++ Assets/Scripts/Physics/Simulator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulator : Singleton<Simulator>
+{
+    public List<Force> forces;
+
+
+    public List<Body> bodies { get; set; } = new List<Body>();
+    Camera activeCamera;
+
+    private void Start()
+    {
+        activeCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        forces.ForEach(force => force.ApplyForce(bodies));
+        //bodies.ForEach(body => Integrator.ExplicitEuler(body, Time.deltaTime));
+        bodies.ForEach(body =>
+        {
+            Integrator.SemiImplicitEuler(body, Time.deltaTime);
+        });
+        bodies.ForEach(body => body.acceleration = Vector2.zero);
+    }
+
+    public Vector3 GetScreenToWorldPosition(Vector2 screen)
+    {
+        Vector3 world = activeCamera.ScreenToWorldPoint(screen);
+        return new Vector3(world.x, world.y, 0);
+    }
+}
---
+++ Assets/Scripts/Shapes/Shape.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Shape : MonoBehaviour
+{
+    public abstract float size { get; set; }
+    public abstract float area { get; }
+    public float mass => area * density;
+    public float density { get; set; } = 1;
+}
